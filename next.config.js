5c424a8df40f8d87b883fa2dcaeae79f1d607da3
GRZEMA <dawidkrzmnsk@gmail.com>
2023-08-18 17:04:35 +0200

    build

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	reactStrictMode: true,
+	env: {
+		NEXTAUTH_SECRET: 'ifQgNLkf/QrKVmTSdSjexkj+sH9sAdckDuLHXRFrdkA=',
+		DB_LOGIN: 'dawidkrzmnsk',
+		DB_PASS: '5O3LDanAeUDkW7Vt',
+	},
 	images: {
 		remotePatterns: [
 			{
