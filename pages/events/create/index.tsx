a33775d8aac31f0c0fbf5e0c23195ae85157e14a
GRZEMA <dawidkrzmnsk@gmail.com>
2023-08-21 20:04:10 +0200

    Secured event creation route

--- pages/events/create/index.tsx
+++ pages/events/create/index.tsx
@@ -1,7 +1,18 @@
 import CreateEvent from '@/components/CreateEvent/CreateEvent'
 import Head from 'next/head'
+import { useRouter } from 'next/router'
+import { useSession } from 'next-auth/react'
+import NotFound from '@/components/404/NotFound'
 
 const CreateEventPage = (): JSX.Element => {
+	const router = useRouter()
+	const session = useSession()
+
+	if (session.status === 'unauthenticated') {
+		router.push('/auth')
+		return <NotFound />
+	}
+
 	return (
 		<>
 			<Head>
